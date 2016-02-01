984970483e80f8bb456a5425f1792970f5fd1437
墨宇 <zzh_hz@126.com>
2016-02-01 14:36:47 +0800

    zzh:测试VAE提交

---
+++ app/src/main/java/com/zzh/vae/base/BaseActivity.java
@@ -0,0 +1,76 @@
+package com.zzh.vae.base;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * Created by zzh on 2016/1/29.
+ * 对BaseActivity的一些封装：
+ * 全局Context
+ * Handler
+ * Toast
+ * 初始化View控件
+ * 初始化数据
+ * 给控件设置监听事件
+ */
+public abstract class BaseActivity extends AppCompatActivity implements View.OnClickListener {
+    protected Context mContext;
+    protected BaseHandler mHandler;
+    protected Toast mToast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = this;
+        mHandler = new BaseHandler();
+    }
+
+    /**
+     * 在子Activity中处理handler
+     *
+     * @param msg
+     */
+    protected abstract void handlerMessage(Message msg);
+
+    /**
+     * 初始化控件使用
+     */
+    protected abstract void initView();
+
+    /**
+     * 初始化数据
+     */
+    protected abstract void initData();
+
+    /**
+     * 设置监听事件
+     */
+    protected abstract void initSetListener();
+
+    public class BaseHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            handlerMessage(msg);
+        }
+    }
+
+    /**
+     * 显示的文字提示信息
+     * @param str
+     */
+    protected void showMessage(String str) {
+        if (mToast == null) {
+            mToast = Toast.makeText(mContext, str, Toast.LENGTH_SHORT);
+            mToast.setGravity(Gravity.CENTER, 0, 0);
+        } else {
+            mToast.setText(str);
+        }
+        mToast.show();
+    }
+}
