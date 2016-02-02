91c205427d25ca1c2023e69882e50fe3de22ba2c
墨宇 <zzh_hz@126.com>
2016-02-02 16:51:13 +0800

    zzh:侧滑菜单的实现

--- app/src/main/java/com/zzh/vae/MainActivity.java
+++ app/src/main/java/com/zzh/vae/MainActivity.java
@@ -3,14 +3,11 @@ package com.zzh.vae;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Message;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.zzh.vae.activity.SlidingActivity;
 import com.zzh.vae.activity.SurfaceActivity;
 import com.zzh.vae.base.BaseActivity;
 
@@ -20,17 +17,6 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_SHORT)
-                        .setAction("Action", null).show();
-            }
-        });
         initView();
         initData();
         initSetListener();
@@ -53,7 +39,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initSetListener() {
-
+        findViewById(R.id.button).setOnClickListener(this);
     }
 
     @Override
@@ -77,6 +63,12 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId())
+        {
+            case R.id.button:
+                Intent intent = new Intent(mContext, SlidingActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
