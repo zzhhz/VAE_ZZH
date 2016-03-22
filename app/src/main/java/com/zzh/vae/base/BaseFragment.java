89c2f507e36d7fcf83e81faa898e26404a91bbf5
墨宇 <zzh_hz@126.com>
2016-03-22 11:50:36 +0800

    BaseFragment

--- app/src/main/java/com/zzh/vae/base/BaseFragment.java
+++ app/src/main/java/com/zzh/vae/base/BaseFragment.java
@@ -1,6 +1,8 @@
 package com.zzh.vae.base;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,9 +13,11 @@ import android.view.ViewGroup;
  * Created by zzh on 2016/3/14.
  */
 public abstract class BaseFragment extends Fragment{
+    protected BaseHandler mHandler;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
+        mHandler = new BaseHandler();
         super.onCreate(savedInstanceState);
     }
 
@@ -22,5 +26,21 @@ public abstract class BaseFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return super.onCreateView(inflater, container, savedInstanceState);
     }
+
+    protected abstract void initView(View fragment);
+    protected abstract void initData();
+
+    private class BaseHandler extends Handler{
+        @Override
+        public void handleMessage(Message msg) {
+            handlerMessage(msg);
+        }
+    }
+
+    /**
+     * 处理消息
+     * @param msg 发送消息
+     */
+    protected abstract void handlerMessage(Message msg);
 
 }
